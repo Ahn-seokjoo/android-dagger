2a36d4d64d4579a3808d956a00a006e30972c9b8
joo <tjrwn0716@naver.com>
2023-03-19 20:10:45 +0900

    [3-3] SubComponent 10번까지

--- app/src/main/java/com/example/android/dagger/di/AppComponent.kt
+++ app/src/main/java/com/example/android/dagger/di/AppComponent.kt
@@ -2,11 +2,12 @@ package com.example.android.dagger.di
 
 import android.content.Context
 import com.example.android.dagger.main.MainActivity
-import com.example.android.dagger.registration.RegistrationActivity
 import dagger.BindsInstance
 import dagger.Component
+import javax.inject.Singleton
 
-@Component(modules = [StorageModule::class])
+@Singleton
+@Component(modules = [StorageModule::class, AppSubComponent::class])
 interface AppComponent {
 
     @Component.Factory
@@ -14,6 +15,6 @@ interface AppComponent {
         fun create(@BindsInstance context: Context): AppComponent
     }
 
-    fun inject(activity: RegistrationActivity)
     fun inject(activity: MainActivity)
+    fun registrationComponent(): RegistrationComponent.Factory
 }
