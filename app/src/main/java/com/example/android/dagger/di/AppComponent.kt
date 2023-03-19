75a3df0cd0ded8adaacf4ee4aed622118d46677d
joo <tjrwn0716@naver.com>
2023-03-19 20:42:47 +0900

    [3-6] 13 ㅂㅓㄴㄲㅏㅈㅣ

--- app/src/main/java/com/example/android/dagger/di/AppComponent.kt
+++ app/src/main/java/com/example/android/dagger/di/AppComponent.kt
@@ -2,6 +2,8 @@ package com.example.android.dagger.di
 
 import android.content.Context
 import com.example.android.dagger.main.MainActivity
+import com.example.android.dagger.settings.SettingsActivity
+import com.example.android.dagger.user.UserManager
 import dagger.BindsInstance
 import dagger.Component
 import javax.inject.Singleton
@@ -14,8 +16,8 @@ interface AppComponent {
     interface Factory {
         fun create(@BindsInstance context: Context): AppComponent
     }
+    fun userManager(): UserManager
 
-    fun inject(activity: MainActivity)
     fun registrationComponent(): RegistrationComponent.Factory
     fun loginComponent(): LoginComponent.Factory
 }
